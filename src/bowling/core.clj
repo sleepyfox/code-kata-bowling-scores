b1bc4dc3dfc5ed995eb85ecf3bc5f3ee0e958e97
@sleepyfox <sleepyfox@gmail.com>
2021-10-29 17:57:55 +0100

    refactor to extract function for score-basic-game in preparation for including spare bonus

--- src/bowling/core.clj
+++ src/bowling/core.clj
@@ -6,9 +6,14 @@
   [frame]
   (reduce + frame))
 
-(defn score-game
+(defn score-basic-game
   "total score for ten frames, absent bonuses"
   [game]
   (->> game
        (map score-frame)
        (reduce +)))
+
+(defn score-game
+  "Score the basic game plus spare bonus rolls"
+  [game]
+  (score-basic-game game))
